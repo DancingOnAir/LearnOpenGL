59f23657ecee7102ebb30e7f5ede505a04c884f7
DancingOnAir <taotong1984@hotmail.com>
2019-08-16 23:04:45 +0800

    feat<CoordinateSystem.cpp>

---
+++ LearnOpenGL/LearnOpenGL/1.4_CreateTriangle.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//
+//
+//int main()
+//{
+//    float vertices[] = { 
+//        -0.5f, -0.5f, 0.0f, 
+//         0.5f, -0.5f, 0.0f,
+//         0.0f, -0.5f, 0.0f
+//    };
+//
+//    unsigned int VBO;
+//    glGenBuffers(1, &VBO);
+//
+//    return 0;
+//}
---
+++ LearnOpenGL/LearnOpenGL/shader_m.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
+    {
+
+    }
+};
